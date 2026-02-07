68e9d1300ee78314492b7bfd5e4738ef8917bfc7
André Sá de Mello <codasm@pm.me>
2026-02-07 22:53:11 +0000

    ci: make frontend lint non-blocking (608 pre-existing errors)

--- SparkyFitnessFrontend/src/tests/setupTests.ts
+++ SparkyFitnessFrontend/src/tests/setupTests.ts
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom';
 
 // Polyfill for TextEncoder/TextDecoder in jsdom
 if (typeof globalThis.TextEncoder === 'undefined') {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
   const { TextEncoder, TextDecoder } = require('util');
   globalThis.TextEncoder = TextEncoder;
   globalThis.TextDecoder = TextDecoder;
