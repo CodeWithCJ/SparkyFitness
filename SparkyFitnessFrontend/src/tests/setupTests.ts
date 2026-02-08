3d5ee1e91a1526b469858cef80c4b2802e55a42b
Simon <git@sattelberger.me>
2026-02-08 15:48:30 +0100

    fix (test): add matchMedia to test env for useisMobile hook

--- SparkyFitnessFrontend/src/tests/setupTests.ts
+++ SparkyFitnessFrontend/src/tests/setupTests.ts
@@ -1,9 +1,24 @@
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
 
 // Polyfill for TextEncoder/TextDecoder in jsdom
-if (typeof globalThis.TextEncoder === 'undefined') {
+if (typeof globalThis.TextEncoder === "undefined") {
   // eslint-disable-next-line @typescript-eslint/no-require-imports
-  const { TextEncoder, TextDecoder } = require('util');
+  const { TextEncoder, TextDecoder } = require("util");
   globalThis.TextEncoder = TextEncoder;
   globalThis.TextDecoder = TextDecoder;
 }
+
+// needed for useisMobile hook
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query: any) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => {},
+  }),
+});
