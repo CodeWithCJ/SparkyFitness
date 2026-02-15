69c00778cccadcda4c5e375b464e66baeb33bae0
codewithcj001 <codewithcj001@gmail.com>
2026-02-15 14:45:29 -0500

    migraiton file optimization

--- SparkyFitnessServer/db/migrations/20260215144500_add_hevy_provider_type.sql
+++ SparkyFitnessServer/db/migrations/20260215144500_add_hevy_provider_type.sql
@@ -3,3 +3,9 @@
 INSERT INTO public.external_provider_types (id, display_name, description)
 VALUES ('hevy', 'Hevy', 'Workout tracking app integration via API Key')
 ON CONFLICT (id) DO NOTHING;
+
+
+
+ALTER TABLE public.exercise_entry_sets ADD COLUMN rpe numeric(3, 1);
+
+COMMENT ON COLUMN public.exercise_entry_sets.rpe IS 'Rate of Perceived Exertion (usually 1-10 scale)';
