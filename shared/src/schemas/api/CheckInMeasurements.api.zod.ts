0e6d50aa300750c7837f23d53bd7610ab83b64ab
Simon <git@sattelberger.me>
2026-03-11 17:09:18 +0100

    fix recentCheckInMeasurements

--- shared/src/schemas/api/CheckInMeasurements.api.zod.ts
+++ shared/src/schemas/api/CheckInMeasurements.api.zod.ts
@@ -12,6 +12,17 @@ export const checkInMeasurementsResponseSchema = checkInMeasurementsSchema
   .omit({
     created_at: true,
   });
+
+export const recentCheckInMeasurementsSchema = z.object({
+  weight: z.number().nullish(),
+  neck: z.number().nullish(),
+  waist: z.number().nullish(),
+  hips: z.number().nullish(),
+  steps: z.number().nullish(),
+  height: z.number().nullish(),
+  body_fat_percentage: z.number().nullish(),
+});
+
 export const updateCheckInMeasurementsRequestSchema =
   checkInMeasurementsMutatorSchema
     .extend({
@@ -21,6 +32,10 @@ export const updateCheckInMeasurementsRequestSchema =
       created_at: true,
       updated_at: true,
     });
+
+export type RecentCheckInMeasurementsResponse = z.infer<
+  typeof recentCheckInMeasurementsSchema
+>;
 export type CheckInMeasurementsResponse = z.infer<
   typeof checkInMeasurementsResponseSchema
 >;
