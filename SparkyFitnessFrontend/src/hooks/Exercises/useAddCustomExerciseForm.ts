8570839b73fc3e502f32fe890cc80a3bf4293a21
Simsat <github@sattelberger.me>
2026-04-06 08:15:41 +0200

    refactor: split exerciseSearch into custom hook

--- SparkyFitnessFrontend/src/hooks/Exercises/useAddCustomExerciseForm.ts
+++ SparkyFitnessFrontend/src/hooks/Exercises/useAddCustomExerciseForm.ts
@@ -1,4 +1,3 @@
-// hooks/Exercises/useAddCustomExerciseForm.ts
 import { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { toast } from '@/hooks/use-toast';
