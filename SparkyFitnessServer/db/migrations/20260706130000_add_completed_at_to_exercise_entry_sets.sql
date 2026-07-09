e0c92a21c80b8c7ea98feec2930cacdb8ffc88ad
apedley <apedley@gmail.com>
2026-07-08 22:05:24 -0500

    refactor: combine db migrations

--- SparkyFitnessServer/db/migrations/20260706130000_add_completed_at_to_exercise_entry_sets.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE public.exercise_entry_sets ADD COLUMN completed_at timestamptz;
-COMMENT ON COLUMN public.exercise_entry_sets.completed_at IS
-  'Client-recorded moment the set was checked off during a live workout. NULL = not completed.';
